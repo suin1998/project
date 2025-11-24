02a28479e2c50117c0fbd39967695eeab5122040
ksi <lsu3097@naver.com>
2025-11-24 10:06:33 +0900

    커밋

---
+++ src/main/java/org/zerock/project/dto/ksi.java
@@ -0,0 +1,7 @@
+package org.zerock.project.dto;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ksi {
+}
---
+++ src/main/java/org/zerock/project/repository/ksi.java
@@ -0,0 +1,7 @@
+package org.zerock.project.repository;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ksi {
+}
