ff10d37cb87edda4abbb86e26d3afab111136a00
ksi <lsu3097@naver.com>
2025-11-27 11:21:48 +0900

    커밋

--- src/main/java/org/zerock/project/controller/ClosetController.java
+++ src/main/java/org/zerock/project/controller/ClosetController.java
@@ -1,34 +1,34 @@
-//package org.zerock.project.controller;
-//
-//import org.zerock.project.domain.Closet;
-//import org.zerock.project.service.ClosetService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/closet")
-//@RequiredArgsConstructor
-//public class ClosetController {
-//    private final ClosetService closetservice;
-//
-//    @PostMapping
-//    public Closet add(@RequestBody Closet closet) {
-//        return closetservice.save(closet);
-//    }
-//
-//    @GetMapping("/{userId}")
-//    public List<Closet> getByUser(@PathVariable String userId) {
-//        return closetservice.getByUserId(userId);
-//    }
-//
-//    @GetMapping("/{userId}/{category}")
-//    public List<Closet> getByCategory(
-//            @PathVariable String userId,
-//            @PathVariable String category
-//    ) {
-//        return closetservice.getByCategory(userId, category);
-//    }
-//}
-//
+package org.zerock.project.controller;
+
+import org.zerock.project.domain.Closet;
+import org.zerock.project.service.ClosetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/closet")
+@RequiredArgsConstructor
+public class ClosetController {
+    private final ClosetService closetservice;
+
+    @PostMapping
+    public Closet add(@RequestBody Closet closet) {
+        return closetservice.save(closet);
+    }
+
+    @GetMapping("/{userId}")
+    public List<Closet> getByUser(@PathVariable String userId) {
+        return closetservice.getByUserId(userId);
+    }
+
+    @GetMapping("/{userId}/{category}")
+    public List<Closet> getByCategory(
+            @PathVariable String userId,
+            @PathVariable String category
+    ) {
+        return closetservice.getByCategory(userId, category);
+    }
+}
+
--- src/main/java/org/zerock/project/domain/Tag.java
+++ src/main/java/org/zerock/project/domain/Tag.java
@@ -1,23 +1,23 @@
-//package org.zerock.project.domain;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//@Entity
-//@Table(name = "tags")
-//@Data
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Tag {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String tag;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "closet_id")
-//    private Closet closet;
-//}
-//
+package org.zerock.project.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tags")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tag;
+
+    @ManyToOne
+    @JoinColumn(name = "closet_id")
+    private Closet closet;
+}
+
