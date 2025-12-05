a925e355e08271a6631af842b37e023e6e4a94f2
ksi <lsu3097@naver.com>
2025-12-05 09:03:56 +0900

    커밋

--- src/main/java/org/zerock/project/dto/OutfitRequestDto.java
+++ src/main/java/org/zerock/project/dto/OutfitRequestDto.java
@@ -1,6 +1,7 @@
 package org.zerock.project.dto;
 
 import com.nimbusds.openid.connect.sdk.claims.Gender;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,7 +16,9 @@ import java.util.List;
 public class OutfitRequestDto {
     public WeatherResponseDto weatherResponseDto;
     public WeatherRequestDto weatherRequestDto;
+    @NotBlank
     private String userId;
+
     private List<String> fashionStyle;
     private List<String> tempStyle;
     private String tpo;
